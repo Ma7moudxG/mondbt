7d844278789eab402166dd471b516a6427e30777
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2025-08-15 11:04:02 +0300

    cards admn

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -487,7 +487,7 @@ const AdminPage = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-2 justify-between flex-wrap">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-2 justify-between flex-wrap">
         {cards.map((i) => (
           <UserCard key={i.type} type={i} />
         ))}
