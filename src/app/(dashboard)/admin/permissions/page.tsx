bc17683842bb6a22c2bf694666a61987dc4b851d
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2025-08-15 11:33:46 +0300

    late 2

--- src/app/(dashboard)/admin/permissions/page.tsx
+++ src/app/(dashboard)/admin/permissions/page.tsx
@@ -209,8 +209,8 @@ const LateStatisticsPage = () => {
   return (
     <div className="p-4 flex flex-col gap-4">
       <div className="flex gap-12">
-        <h1 className="text-lg font-black text-[#7C8B9D]">
-          {getConsistentTranslatedText("Late")}
+        <h1 className="text-xl font-black text-[#7C8B9D]">
+          {getConsistentTranslatedText("Permissions")}
         </h1>
       </div>
 
